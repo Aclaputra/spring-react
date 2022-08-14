190458d0a9248a15752b71f2270dcb67969ca74f
Aclaputra <muhamadacla22@gmail.com>
2022-08-14 14:39:23 +0700

    feat: clean the table css a bit

--- react-frontend/src/components/TableRow.jsx
+++ react-frontend/src/components/TableRow.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+import './css/TableRow.css'
 // import TableActionButtons from './TableActionButtons';
 
 export default class TableRow extends Component {
@@ -11,11 +13,14 @@ export default class TableRow extends Component {
         return (
             <>
                 <tr>
-                    <th scope="row">{this.props.data.id}</th>
-                    <td>{this.props.data.name}</td>
-                    <td>{this.props.data.quantity}</td>
-                    <td>{this.props.data.price}</td>
-                    <td>
+                    <th scope="row" style={{padding: '10px'}}>{this.props.data.id}</th>
+                    <td style={{padding: '10px'}}>{this.props.data.name}</td>
+                    <td style={{padding: '10px'}}>{this.props.data.quantity}</td>
+                    <td style={{padding: '10px'}}>{this.props.data.price}</td>
+                    <td style={{padding: '10px'}}>{this.props.data.position}</td>
+                    <td style={{padding: '10px'}}>{this.props.data.salary}</td>
+                    <td style={{padding: '10px'}}>
+                        <Button variant="primary">Primary</Button>{' '}
                         {/* <TableActionButtons eachRowId={this.props.data.id} /> */}
                     </td>   
                 </tr>
