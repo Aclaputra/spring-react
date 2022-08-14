244e00b0ec23cabf23c088c54837c78c308f56b8
Aclaputra <muhamadacla22@gmail.com>
2022-08-14 14:38:50 +0700

    feata: added getEmployeeList function and run it on componentDidMount

--- react-frontend/src/components/Employee.jsx
+++ react-frontend/src/components/Employee.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { Component } from 'react'
+import TableRow from './TableRow';
 
 export default class Employee extends Component {
 
@@ -11,13 +13,26 @@ export default class Employee extends Component {
     }
 
     componentDidMount() {
+      this.getEmployeeList();
+    }
 
+    getEmployeeList = () => {
+      let self = this;
+      axios.get('http://localhost:9191/employees').then(function(response) {
+        self.setState({
+          employees: response.data
+        });
+      })
     }
 
     
   render() {
     return (
-      <div>Employee</div>
+      <div>
+        {this.state.employees.map(function(x, i) {
+          return <TableRow key={i} data={x} />
+        })}
+      </div>
     )
   }
 }
