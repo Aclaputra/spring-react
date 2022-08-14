2042d95b72f8e4b71cc46ef58b9383dd61fe4ae9
Aclaputra <muhamadacla22@gmail.com>
2022-08-14 14:38:07 +0700

    feat: added employee and modified inside a div on app.jsx

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
+import Employee from './components/Employee';
 import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
-        <Product/>
+        <div style={{padding: '10px'}}>
+          <Product/>
+        </div>
+        <div style={{padding: '10px'}}>
+          <Employee/>
+        </div>
     </div>
   );
 }
-
 export default App;
