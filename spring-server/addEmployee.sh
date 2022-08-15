e8c9c6a982bcbf855d25a48493d2b1de6f9644a2
Aclaputra <muhamadacla22@gmail.com>
2022-08-16 00:35:22 +0700

    delete: deleted addEmployee.sh

--- spring-server/addEmployee.sh
+++
@@ -1,7 +0,0 @@
-curl --location --request POST 'http://localhost:9191/addemployee' \
-  --header 'Content-Type: application/json' \
-  --data-raw '{
-      "name": "Athaya",
-      "position": "Backend Engineer Intern",
-      "salary": 13000
-  }'
