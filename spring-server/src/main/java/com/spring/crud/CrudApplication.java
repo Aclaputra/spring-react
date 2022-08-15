813ee160b7c620db8b9eb518bf167e587706ff8d
Aclaputra <muhamadacla22@gmail.com>
2022-08-15 17:08:51 +0700

    feat: added comment to crudapplication

--- spring-server/src/main/java/com/spring/crud/CrudApplication.java
+++ spring-server/src/main/java/com/spring/crud/CrudApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 
+  // main class or core function that will run everything
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
