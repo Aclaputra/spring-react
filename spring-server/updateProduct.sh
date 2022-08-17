309173f66d4d543656f4d19b541ee6883533c95d
Aclaputra <muhamadacla22@gmail.com>
2022-08-17 16:45:50 +0700

    migrate updateProduct

--- spring-server/updateProduct.sh
+++
@@ -1,8 +0,0 @@
-curl --location --request PUT 'http://localhost:9191/updateProduct' \
-  --header 'Content-Type: application/json' \
-  --data-raw '{
-      "id": 4,
-      "name": "macbook",
-      "quantity": 99,
-      "price": 23213
-  }'
