d7b2944f46ee66526e5ca21a65fd0fd674d04b8e
Aclaputra <muhamadacla22@gmail.com>
2022-08-17 16:41:51 +0700

    migrate addProduct

--- spring-server/addProduct.sh
+++
@@ -1,7 +0,0 @@
-curl --location --request POST 'http://localhost:9191/addproduct' \
-  --header 'Content-Type: application/json' \
-  --data-raw '{
-    "name": "Keyboard",
-        "quantity": 52,
-            "price": 29000
-}'
